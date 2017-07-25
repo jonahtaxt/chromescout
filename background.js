6b0a5438b0cf2c3b19204cdd3a0a68921f019665
Jorge Aguilar <jorgea.aguilarh@gmail.com>
2017-07-25 16:38:12 -0500

    Used chrome alarm to refresh data

--- background.js
+++ background.js
@@ -1,15 +1,63 @@
-var nsUrl = '';
-chrome.webNavigation.onCompleted.addListener(function(details){
-	chrome.storage.sync.get({
-		"nightscoutUrl" : 'https://<yoursite>.azurewebsites.net/'
-	}, function(items) {
-		if(items.nightscoutUrl === 'https://<yoursite>.azurewebsites.net/') {
-			$('#setupExtension').css('display','block');
-			$('#bgData').css('display','none');
-		} else {
-			nsUrl = items.nightscoutUrl;
-			$('#setupExtension').css('display','none');
-			$('#bgData').css('display','block');
-		}
-	});
+var nsVars = {};
+const nsGetDataAlarmName = "refreshNSData";
+
+function getLastBGReading() {
+    $.get(nsVars.nsUrl + 'api/v1/entries.json?count=2', function (data) {
+        nsVars.lastBGReadingInfo = data[0];
+        nsVars.priorBGReadingInfo = data[1];
+        nsVars.currentBGReading = nsVars.lastBGReadingInfo.sgv;
+        nsVars.trend = nsVars.lastBGReadingInfo.sgv - nsVars.priorBGReadingInfo.sgv;
+        nsVars.dataLoaded = true;
+
+        chrome.storage.sync.set({ "extensionVars": nsVars });
+    });
+}
+
+function initialize() {
+    nsVars = {
+        nsUrl: null,
+        currentSystemStatus: null,
+        thresholds: null,
+        lastBGReadingInfo: null,
+        priorBGReadingInfo: null,
+        currentBGReading: null,
+        trend: null,
+        dataLoaded: false
+    };
+}
+
+function getSystemStatus() {
+    chrome.storage.sync.get({
+        "nightscoutUrl": 'https://<yoursite>.azurewebsites.net/'
+    }, function (items) {
+        initialize(items);
+        if (!(items.nightscoutUrl === 'https://<yoursite>.azurewebsites.net/')) {
+            nsVars.nsUrl = items.nightscoutUrl;
+            $.get(nsVars.nsUrl + 'api/v1/status.json', function (data) {
+                nsVars.currentSystemStatus = data;
+                nsVars.thresholds = nsVars.currentSystemStatus.settings.thresholds;
+                getLastBGReading();
+            });
+        }
+    });
+}
+
+chrome.alarms.getAll(function (alarms) {
+    var alarmExists = false;
+    for (var i = 0; i < alarms.length; i++) {
+        if (alarms[i].name === nsGetDataAlarmName) {
+            alarmExists = true;
+            break;
+        }
+    }
+    if (!alarmExists) {
+        getSystemStatus();
+        chrome.alarms.create(nsGetDataAlarmName, { delayInMinutes: 5, periodInMinutes: 5 });
+    }
+});
+
+chrome.alarms.onAlarm.addListener(function (alarm) {
+    if (alarm.name === nsGetDataAlarmName) {
+        getSystemStatus();
+    }
 });
--- popup.js
+++ popup.js
@@ -1,80 +1,85 @@
-var currentSystemStatus;
-var thresholds;
-var nsUrl;
+var backgroundVars = null;
 
 function sleep(ms) {
-	return new Promise(resolve => setTimeout(resolve, ms));
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 function calculateSleepTime(remainingPoints) {
-	if(remainingPoints <= 25) {
-		return 50;
-	} else if (remainingPoints <= 10) {
-		return 200;
-	} else {
-		return 10;
-	}
+    if (remainingPoints <= 25) {
+        return 50;
+    } else if (remainingPoints <= 10) {
+        return 200;
+    } else {
+        return 10;
+    }
 }
 
 function setBGReadingColor(currentBGPoint) {
-	var currentColor = '';
-	
-	if(currentBGPoint <= thresholds.bgLow || currentBGPoint >= thresholds.bgHigh) {
-		currentColor = 'red';
-	} else if ((currentBGPoint > thresholds.bgLow && currentBGPoint <= thresholds.bgTargetBottom) ||
-	(currentBGPoint >= thresholds.bgTargetTop && currentBGPoint < thresholds.bgHigh)) {
-		currentColor = 'yellow';
-	} else {
-		currentColor = '#4cff00';
-	}
-	return currentColor;
+    var currentColor = '';
+
+    if (currentBGPoint <= backgroundVars.thresholds.bgLow || currentBGPoint >= backgroundVars.thresholds.bgHigh) {
+        currentColor = 'red';
+    } else if ((currentBGPoint > backgroundVars.thresholds.bgLow && currentBGPoint <= backgroundVars.thresholds.bgTargetBottom) ||
+        (currentBGPoint >= backgroundVars.thresholds.bgTargetTop && currentBGPoint < backgroundVars.thresholds.bgHigh)) {
+        currentColor = 'yellow';
+    } else {
+        currentColor = '#4cff00';
+    }
+    return currentColor;
 }
 
-async function animateLastBGReading(bgReadingSpan, bgReading){
-	bgReadingSpan.text('0');
-	for(var x = 0; x <= bgReading; x++) {
-		bgReadingSpan.text(x.toString());
-		bgReadingSpan.css('color', setBGReadingColor(x));		
-		await sleep(calculateSleepTime(bgReading - x));
-	}
+async function animateLastBGReading(bgReadingSpan, bgReading) {
+    bgReadingSpan.text('0');
+    for (var x = 0; x <= bgReading; x++) {
+        bgReadingSpan.text(x.toString());
+        bgReadingSpan.css('color', setBGReadingColor(x));
+        await sleep(calculateSleepTime(bgReading - x));
+    }
 }
 
-function getLastBGReading() {
-	var lastBGReadingSpan = $("#lastBGReading");
-	var trendSpan = $("#trend");
-	$.get(nsUrl + 'api/v1/entries.json?count=2', function(data){
-		var lastBGReadingInfo = data[0];
-		var priorBGReadingInfo = data[1];
-		var currentBGReading = lastBGReadingInfo.sgv;
-		var trend = lastBGReadingInfo.sgv - priorBGReadingInfo.sgv;
-		animateLastBGReading(lastBGReadingSpan, currentBGReading);
-		if(trend < 0) {
-			trendSpan.text(trend.toString());			
-		} else {
-			trendSpan.text('+ ' + trend.toString());
-		}
-	});
+function initialize() {
+
+    chrome.storage.sync.get({
+        "extensionVars": null
+    }, function (items) {
+        backgroundVars = items.extensionVars;
+
+        var setupExtensionDiv = $('#setupExtension');
+        var retrieveDataDiv = $('#retrieveData');
+        var bgDataDiv = $('#bgData');
+
+        if (backgroundVars !== null) {
+            if (backgroundVars.nsUrl === 'https://<yoursite>.azurewebsites.net/') {
+                setupExtensionDiv.show();
+                retrieveDataDiv.hide();
+                bgDataDiv.hide();
+                return;
+            }
+
+            if (!backgroundVars.dataLoaded) {
+                setupExtensionDiv.hide();
+                retrieveDataDiv.show();
+                bgDataDiv.hide();
+                return;
+            }
+            else {
+                setupExtensionDiv.hide();
+                retrieveDataDiv.hide();
+                bgDataDiv.show();
+
+                var lastBGReadingSpan = $("#lastBGReading");
+                var trendSpan = $("#trend");
+                animateLastBGReading(lastBGReadingSpan, backgroundVars.currentBGReading);
+                if (backgroundVars.trend < 0) {
+                    trendSpan.text(backgroundVars.trend.toString());
+                } else {
+                    trendSpan.text('+ ' + backgroundVars.trend.toString());
+                }
+            }
+        }
+    });
 }
 
-function getSystemStatus() {
-	var backgroundVars = chrome.extension.getBackgroundPage();
-	nsUrl = backgroundVars.nsUrl;
-	if(nsUrl === 'https://<yoursite>.azurewebsites.net/') {
-		$('#setupExtension').css('display','block');
-		$('#bgData').css('display','none');
-		return;
-	} else {
-		$('#setupExtension').css('display','none');
-		$('#bgData').css('display','block');
-	}
-	$.get(nsUrl + 'api/v1/status.json', function(data) {
-		currentSystemStatus = data;
-		thresholds = currentSystemStatus.settings.thresholds;
-		$("#userTitle").text(currentSystemStatus.settings.customTitle);
-		getLastBGReading();
-	});
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-	getSystemStatus();
+document.addEventListener('DOMContentLoaded', function () {
+    initialize();
 });
