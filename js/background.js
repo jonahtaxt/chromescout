2cc237b500f0fe36afd6fd465f79fc17bd79c047
Jorge Aguilar <jorgea.aguilarh@gmail.com>
2017-07-31 19:22:29 -0500

    Fix # 10 change icon color depending on current bg

--- js/background.js
+++ js/background.js
@@ -1,6 +1,20 @@
 var nsVars = {};
 var nsGetDataAlarmName = 'refreshNSData';
 
+function setBGReadingIconColor(currentBGPoint) {
+    var currentIconColor = '';
+
+    if (currentBGPoint <= nsVars.thresholds.bgLow || currentBGPoint >= nsVars.thresholds.bgHigh) {
+        currentIconColor = 'logored.png';
+    } else if ((currentBGPoint > nsVars.thresholds.bgLow && currentBGPoint <= nsVars.thresholds.bgTargetBottom) ||
+        (currentBGPoint >= nsVars.thresholds.bgTargetTop && currentBGPoint < nsVars.thresholds.bgHigh)) {
+        currentIconColor = 'logoyellow.png';
+    } else {
+        currentIconColor = 'logo.png';
+    }
+    return currentIconColor;
+}
+
 function getLastBGReading() {
     $.get(nsVars.nsUrl + 'api/v1/entries.json?count=2', function (data) {
         nsVars.lastBGReadingInfo = data[0];
@@ -22,6 +36,12 @@ function getLastBGReading() {
 		newIconTitle += ')';
 		
 		chrome.browserAction.setTitle({title: newIconTitle});
+		
+		var icon = {
+			path: '../img/' + setBGReadingIconColor(nsVars.currentBGReading)
+		};
+		
+		chrome.browserAction.setIcon(icon);
     });
 }
 
@@ -56,16 +76,16 @@ function initialize() {
 
 document.addEventListener('DOMContentLoaded', function(){
 	chrome.alarms.clear(nsGetDataAlarmName, function(wasCleared) {
-	
-	chrome.alarms.create(nsGetDataAlarmName, { delayInMinutes: 5, periodInMinutes: 5 });
-	chrome.alarms.onAlarm.addListener(function (alarm) {
-		if (alarm.name === nsGetDataAlarmName) {
-			chrome.storage.sync.get({
-				"nightscoutUrl": "https://<yoursite>.azurewebsites.net/"
-			}, function (items) {
-				initialize();
-			});
-		}
+		chrome.alarms.create(nsGetDataAlarmName, { delayInMinutes: 5, periodInMinutes: 5 });
 	});
 });
+
+chrome.alarms.onAlarm.addListener(function (alarm) {
+	if (alarm.name === nsGetDataAlarmName) {
+		chrome.storage.sync.get({
+			"nightscoutUrl": "https://<yoursite>.azurewebsites.net/"
+		}, function (items) {
+			initialize();
+		});
+	}
 });
