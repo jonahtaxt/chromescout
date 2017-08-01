68bba3bbbccf8626ba7708aabf8aaa021ffd8a68
Jorge Aguilar <jorgea.aguilarh@gmail.com>
2017-08-01 11:49:35 -0500

    Fix #17 extension automatically tries to get bg data

--- js/background.js
+++ js/background.js
@@ -1,91 +1,102 @@
-var nsVars = {};
-var nsGetDataAlarmName = 'refreshNSData';
+var self = this;
 
-function setBGReadingIconColor(currentBGPoint) {
-    var currentIconColor = '';
+self.nsVars = {};
+self.nsGetDataAlarmName = 'refreshNSData';
 
-    if (currentBGPoint <= nsVars.thresholds.bgLow || currentBGPoint >= nsVars.thresholds.bgHigh) {
-        currentIconColor = 'logored.png';
-    } else if ((currentBGPoint > nsVars.thresholds.bgLow && currentBGPoint <= nsVars.thresholds.bgTargetBottom) ||
-        (currentBGPoint >= nsVars.thresholds.bgTargetTop && currentBGPoint < nsVars.thresholds.bgHigh)) {
-        currentIconColor = 'logoyellow.png';
-    } else {
-        currentIconColor = 'logo.png';
-    }
-    return currentIconColor;
-}
+self.setBGReadingIconColor = function (currentBGPoint) {
+	var currentIconColor = '';
 
-function getLastBGReading() {
-    $.get(nsVars.nsUrl + 'api/v1/entries.json?count=2', function (data) {
-        nsVars.lastBGReadingInfo = data[0];
-        nsVars.priorBGReadingInfo = data[1];
-        nsVars.currentBGReading = nsVars.lastBGReadingInfo.sgv;
-        nsVars.delta = nsVars.lastBGReadingInfo.sgv - nsVars.priorBGReadingInfo.sgv;
-        nsVars.dataLoaded = true;
+	if (currentBGPoint <= self.nsVars.thresholds.bgLow || currentBGPoint >= self.nsVars.thresholds.bgHigh) {
+		currentIconColor = 'logored.png';
+	} else if ((currentBGPoint > self.nsVars.thresholds.bgLow && currentBGPoint <= self.nsVars.thresholds.bgTargetBottom) ||
+		(currentBGPoint >= self.nsVars.thresholds.bgTargetTop && currentBGPoint < self.nsVars.thresholds.bgHigh)) {
+		currentIconColor = 'logoyellow.png';
+	} else {
+		currentIconColor = 'logo.png';
+	}
+	return currentIconColor;
+};
 
-        chrome.storage.sync.set({ "extensionVars": nsVars });
-		
-		var newIconTitle = nsVars.lastBGReadingInfo.sgv.toString() + ' (';
-		
-		if(nsVars.delta >= 0) {
-			newIconTitle += '+ ' + nsVars.delta.toString();
-		} else {
-			newIconTitle += nsVars.delta.toString();
-		}
-		
-		newIconTitle += ')';
-		
-		chrome.browserAction.setTitle({title: newIconTitle});
-		
-		var icon = {
-			path: '../img/' + setBGReadingIconColor(nsVars.currentBGReading)
-		};
-		
-		chrome.browserAction.setIcon(icon);
-    });
-}
+self.setIconTitle = function () {
+	var newIconTitle = self.nsVars.lastBGReadingInfo.sgv.toString() + ' (';
 
-function getCurrentStatus(items) {
+	if (self.nsVars.delta >= 0) {
+		newIconTitle += '+ ' + self.nsVars.delta.toString();
+	} else {
+		newIconTitle += self.nsVars.delta.toString();
+	}
+
+	newIconTitle += ')';
+
+	chrome.browserAction.setTitle({
+		title: newIconTitle
+	});
+};
+
+self.getLastBGReading = function () {
+	$.get(self.nsVars.nsUrl + 'api/v1/entries.json?count=2', function (data) {
+		self.nsVars.lastBGReadingInfo = data[0];
+		self.nsVars.priorBGReadingInfo = data[1];
+		self.nsVars.currentBGReading = self.nsVars.lastBGReadingInfo.sgv;
+		self.nsVars.delta = self.nsVars.lastBGReadingInfo.sgv - self.nsVars.priorBGReadingInfo.sgv;
+		self.nsVars.dataLoaded = true;
+
+		chrome.storage.sync.set({
+			"extensionVars": self.nsVars
+		});
+
+		self.setIconTitle();
+
+		chrome.browserAction.setIcon({
+			path: '../img/' + setBGReadingIconColor(self.nsVars.currentBGReading)
+		});
+	});
+};
+
+self.getCurrentStatus = function (items) {
 	if (!(items.nightscoutUrl === 'https://<yoursite>.azurewebsites.net/')) {
-		nsVars.nsUrl = items.nightscoutUrl;
-		$.get(nsVars.nsUrl + 'api/v1/status.json', function (data) {
-			nsVars.currentSystemStatus = data;
-			nsVars.thresholds = nsVars.currentSystemStatus.settings.thresholds;
-			getLastBGReading();
+		self.nsVars.nsUrl = items.nightscoutUrl;
+		$.get(self.nsVars.nsUrl + 'api/v1/status.json', function (data) {
+			self.nsVars.currentSystemStatus = data;
+			self.nsVars.thresholds = self.nsVars.currentSystemStatus.settings.thresholds;
+			self.getLastBGReading();
 		});
 	}
-}
+};
 
-function initialize() {
-    nsVars = {
-        nsUrl: null,
-        currentSystemStatus: null,
-        thresholds: null,
-        lastBGReadingInfo: null,
-        priorBGReadingInfo: null,
-        currentBGReading: null,
-        delta: null,
-        dataLoaded: false
-    };
-	chrome.storage.sync.get({ 
-        "nightscoutUrl": 'https://<yoursite>.azurewebsites.net/' 
-    }, function(items) {
-		getCurrentStatus(items);
-	});
-}
-
-document.addEventListener('DOMContentLoaded', function(){
-	chrome.alarms.clear(nsGetDataAlarmName, function(wasCleared) {
-		chrome.alarms.create(nsGetDataAlarmName, { delayInMinutes: 5, periodInMinutes: 5 });
-	});
-});
-
-chrome.alarms.onAlarm.addListener(function (alarm) {
-	if (alarm.name === nsGetDataAlarmName) {
-		chrome.storage.sync.get({
+self.initialize = function () {
+	self.nsVars = {
+		nsUrl: null,
+		currentSystemStatus: null,
+		thresholds: null,
+		lastBGReadingInfo: null,
+		priorBGReadingInfo: null,
+		currentBGReading: null,
+		delta: null,
+		dataLoaded: false
+	};
+	
+	chrome.storage.sync.get({
 			"nightscoutUrl": "https://<yoursite>.azurewebsites.net/"
 		}, function (items) {
-			initialize();
+			self.getCurrentStatus(items);
 		});
-	}
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+	chrome.alarms.clear(nsGetDataAlarmName, function (wasCleared) {
+
+		chrome.alarms.create(nsGetDataAlarmName, {
+			delayInMinutes: 5,
+			periodInMinutes: 5
+		});
+
+		chrome.alarms.onAlarm.addListener(function (alarm) {
+			if (alarm.name === nsGetDataAlarmName) {
+				self.initialize();
+			}
+		});
+
+		self.initialize();
+	});
 });
--- js/options.js
+++ js/options.js
@@ -1,26 +1,29 @@
 const nsGetDataAlarmName = 'refreshNSData';
 
 function save_options() {
-    var nsUrl = document.getElementById('nsUrl').value;
-    var statusDiv = document.getElementById('status');
+	var nsUrl = document.getElementById('nsUrl').value;
+	var statusDiv = document.getElementById('status');
 
-    if (nsUrl === 'https://<yoursite>.azurewebsites.net/') {
-        statusDiv.textContent = 'Please enter your Nightscout website Url';
-        setTimeout(function () {
-            statusDiv.textContent = '';
-        }, 2000);
-        return;
-    }
+	if (nsUrl === 'https://<yoursite>.azurewebsites.net/') {
+		statusDiv.textContent = 'Please enter your Nightscout website Url';
+		setTimeout(function () {
+			statusDiv.textContent = '';
+		}, 2000);
+		return;
+	}
 
 	chrome.permissions.request({
 		permissions: ['tabs'],
 		origins: [nsUrl]
-	}, function(granted) {
-		if(granted){
+	}, function (granted) {
+		if (granted) {
 			chrome.storage.sync.set({
 				"nightscoutUrl": nsUrl
 			}, function () {
-				chrome.alarms.create(nsGetDataAlarmName, { delayInMinutes: 5, periodInMinutes: 5 });
+				chrome.alarms.create(nsGetDataAlarmName, {
+					delayInMinutes: 5,
+					periodInMinutes: 5
+				});
 				var status = document.getElementById('status');
 				status.textContent = 'Options saved.';
 				setTimeout(function () {
@@ -30,36 +33,39 @@ function save_options() {
 		} else {
 			var status = document.getElementById('status');
 			status.textContent = 'Please grant permissions to get access to your NS site';
-			setTimeout(function() {
-				status.textContent='';
+			setTimeout(function () {
+				status.textContent = '';
 			}, 2000);
 		}
 	});
 }
 
 function restore_options() {
-    chrome.storage.sync.get({
-        "nightscoutUrl": 'https://<yoursite>.azurewebsites.net/'
-    }, function (items) {
-        if (items.nightscoutUrl === 'https://<yoursite>.azurewebsites.net/') {
-            document.getElementById('clear').disabled = true;
-        } else {
-            document.getElementById('clear').disabled = false;
-        }
-        document.getElementById('nsUrl').value = items.nightscoutUrl;
-    });
+	chrome.storage.sync.get({
+		"nightscoutUrl": 'https://<yoursite>.azurewebsites.net/'
+	}, function (items) {
+		if (items.nightscoutUrl === 'https://<yoursite>.azurewebsites.net/') {
+			document.getElementById('clear').disabled = true;
+		} else {
+			document.getElementById('clear').disabled = false;
+		}
+		document.getElementById('nsUrl').value = items.nightscoutUrl;
+	});
 }
 
 function clear_options() {
-    chrome.storage.sync.clear(function () {
-        chrome.alarms.create(nsGetDataAlarmName, { delayInMinutes: 5, periodInMinutes: 5 });
+	chrome.storage.sync.clear(function () {
+		chrome.alarms.create(nsGetDataAlarmName, {
+			delayInMinutes: 5,
+			periodInMinutes: 5
+		});
 		document.getElementById('nsUrl').value = 'https://<yoursite>.azurewebsites.net/';
 		var status = document.getElementById('status');
 		status.textContent = 'Options cleared.';
 		setTimeout(function () {
 			status.textContent = '';
 		}, 2000);
-    });
+	});
 }
 
 document.addEventListener('DOMContentLoaded', restore_options);
